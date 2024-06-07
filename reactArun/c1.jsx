dd5df025b86a0f3d45f056d6e8d8f4c0056e1783
Arun Rawat <runrawat1@gmail.com>
2024-06-06 21:04:08 -0400

    Add NewComponent to the project

---
+++ reactArun/c1.jsx
@@ -0,0 +1,12 @@
+// src/NewComponent.jsx
+import React from 'react';
+
+const NewComponent = () => {
+    return (
+        <div>
+            <h1>Hello from New Component!</h1>
+        </div>
+    );
+};
+
+export default NewComponent;
